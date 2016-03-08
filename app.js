1fde214808e31e910107b266f80fba96d9ea6169
Immad Mohamed <iMoe037@users.noreply.github.com>
2016-03-08 14:33:49 -0500

    renders

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require( 'express' );
+var app = express();
 var swig = require('swig');
-var app = express(); // creates an instance of an express application
-debugger;
+
+swig.setDefaults({ cache: false });
 
 
 // app.use('/special/',function(request, response) {
@@ -9,15 +10,31 @@ debugger;
 	
 // });
 
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+// swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 app.use(function(request, response, next) {
 	console.log(request.method + request.url);
 	next();
 });
 
-
-
 app.get('/', function(request, response) {
-	response.send('directory');
+	response.render('index',locals);
 });
 app.get('/news', function(request, response) {
 	response.send('news');
